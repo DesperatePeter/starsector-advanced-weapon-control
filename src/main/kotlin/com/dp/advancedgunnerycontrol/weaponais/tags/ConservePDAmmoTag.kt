fed86f6fcc0223653da7925d4c68c4bc580c6ff8
Sean <seanra.busi@pm.me>
2022-09-11 21:27:04 +1000

    added tag

---
+++ src/main/kotlin/com/dp/advancedgunnerycontrol/weaponais/tags/ConservePDAmmoTag.kt
@@ -0,0 +1,30 @@
+package com.dp.advancedgunnerycontrol.weaponais.tags
+
+import com.dp.advancedgunnerycontrol.settings.Settings
+import com.dp.advancedgunnerycontrol.weaponais.*
+import com.fs.starfarer.api.combat.CombatEntityAPI
+import com.fs.starfarer.api.combat.MissileAPI
+import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.combat.WeaponAPI
+import org.lwjgl.util.vector.Vector2f
+
+// Only fire at < maximum ammo if target is PD
+class ConservePDAmmoTag(weapon: WeaponAPI, private val ammoThreshold: Float) : WeaponAITagBase(weapon) {
+
+    override fun isBaseAiValid(entity: CombatEntityAPI): Boolean = true
+
+    override fun computeTargetPriorityModifier(entity: CombatEntityAPI, predictedLocation: Vector2f): Float = 1f
+
+    override fun shouldFire(entity: CombatEntityAPI, predictedLocation: Vector2f): Boolean {
+        if(ammoLevel(weapon) < ammoThreshold)  {
+            return (entity as? ShipAPI)?.isFighter == true || (entity is MissileAPI) && isPD(weapon)
+        }
+        return true
+    }
+
+    override fun isBaseAiOverridable(): Boolean = true
+
+    override fun avoidDebris(): Boolean = false
+
+    override fun isValid(): Boolean = weapon.usesAmmo()
+}
